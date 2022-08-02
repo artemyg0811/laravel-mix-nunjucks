222c26be7f955eadc20dbfd0f5c5628ef05265e0
Artem <artiom.ygryumov@gmail.com>
2022-08-02 19:13:39 +0300

    Add new option path. Update compile files without _. Update run only .njk files. Update watch only .njk files

--- src/NunjucksTask.js
+++ src/NunjucksTask.js
@@ -1,3 +1,8 @@
+// Добавил новую опцию path, чтобы можно было указать путь, откуда будут браться шаблоны
+// Обновил условия компиляции файлов. Файлы которые имеют название или находятся в папке с названием начинающимся с _, не будут попадать в папку с продакшн файлами
+// Компиляция только файлов с расширением .njk
+// Отслеживание изменения только файлов с расширением .njk
+
 const path = require("path");
 const nunjucks = require("nunjucks");
 const fm = require("front-matter");
@@ -9,192 +14,195 @@ const Log = require("laravel-mix/src/Log");
 const NunjucksMixTag = require("./NunjucksMixTag");
 
 class NunjucksTask {
-  /**
-   * Create a new task instance.
-   *
-   * @param {object} data
-   */
-  constructor(data) {
-    this.data = data;
-    this.from = new File(data.from);
-    this.to = new File(data.to);
-    this.assets = [];
-    this.options = Object.assign(
-      {
-        ext: ".html",
-        data: {},
-        block: "content",
-        marked: null,
-        envOptions: null,
-        manageEnv: null,
-      },
-      data.options
-    );
-    this.base = this.from.isDirectory()
-      ? this.from.path()
-      : this.from.segments.base.split("*")[0];
+	/**
+	 * Create a new task instance.
+	 *
+	 * @param {object} data
+	 */
+	constructor(data) {
+		this.data = data;
+		this.from = new File(data.from);
+		this.to = new File(data.to);
+		this.assets = [];
+		this.options = Object.assign(
+			{
+				ext: ".html",
+				data: {},
+				block: "content",
+				marked: null,
+				envOptions: null,
+				manageEnv: null,
+				path: null
+			},
+			data.options
+		);
+		this.base = this.from.isDirectory()
+			? this.from.path()
+			: this.from.segments.base.split("*")[0];
 
-    this.watcher = null;
-    this.isBeingWatched = false;
-    const loader = new nunjucks.FileSystemLoader(this.base, { noCache: true });
-    this.compiler = new nunjucks.Environment(loader, this.options.envOptions);
-    this.compiler.addExtension("mix", new NunjucksMixTag());
-    marked.setOptions(this.options.marked);
+		this.path = this.options.path === null ? this.base : this.options.path
 
-    if (typeof this.options.manageEnv === "function") {
-      this.options.manageEnv.call(null, this.compiler);
-    }
-  }
+		this.watcher = null;
+		this.isBeingWatched = false;
+		const loader = new nunjucks.FileSystemLoader(this.path, { noCache: true });
+		this.compiler = new nunjucks.Environment(loader, this.options.envOptions);
+		this.compiler.addExtension("mix", new NunjucksMixTag());
+		marked.setOptions(this.options.marked);
 
-  /**
-   * Compile template file
-   *
-   * @param {File} srcFile
-   * @returns {File}
-   */
-  getDistFile(srcFile) {
-    let distFile = this.to;
-    if (distFile.isDirectory()) {
-      distFile = distFile.append(
-        path.join(
-          path.relative(this.base, srcFile.base()),
-          srcFile.nameWithoutExtension() + this.options.ext
-        )
-      );
-    }
+		if (typeof this.options.manageEnv === "function") {
+			this.options.manageEnv.call(null, this.compiler);
+		}
+	}
 
-    return distFile;
-  }
+	/**
+	 * Compile template file
+	 *
+	 * @param {File} srcFile
+	 * @returns {File}
+	 */
+	getDistFile(srcFile) {
+		let distFile = this.to;
+		if (distFile.isDirectory()) {
+			distFile = distFile.append(
+				path.join(
+					path.relative(this.base, srcFile.base()),
+					srcFile.nameWithoutExtension() + this.options.ext
+				)
+			);
+		}
 
-  /**
-   * Check whether is partial file
-   *
-   * @param {File} file
-   * @returns {boolean}
-   */
-  isPartialFile(file) {
-    return path
-      .relative(this.base, file.path())
-      .split(path.sep)
-      .some((name) => name.startsWith("_"));
-  }
+		return distFile;
+	}
 
-  /**
-   * Run the task and render all files
-   * except name start with '_'
-   */
-  run() {
-    // glob patterns can only contain forward-slashes, not backward-slashes
-    const patterns = [
-      this.from.path().replace(/\\/g, '/'),
-      "!" + path.posix.join(this.base, `**/_**/**/*`),
-      "!" + path.posix.join(this.base, `**/_*`),
-    ];
+	/**
+	 * Check whether is partial file
+	 *
+	 * @param {File} file
+	 * @returns {boolean}
+	 */
+	isPartialFile(file) {
+		return path
+			.relative(this.base, file.path())
+			.split(path.sep)
+			.some((name) => name.startsWith("_"));
+	}
 
-    const files = globby.sync(patterns, { onlyFiles: true });
-    this.assets = files.map((filePath) => {
-      const srcFile = new File(filePath);
-      const distFile = this.getDistFile(srcFile);
+	/**
+	 * Run the task and render all files
+	 * except name start with '_'
+	 */
+	run() {
+		// glob patterns can only contain forward-slashes, not backward-slashes
+		const patterns = [
+			path.join(this.from.path(), '**/*.njk').replace(/\\/g, '/'),
+			"!" + path.posix.join(this.base, `**/_**/**/*`).replace(/\\/g, '/'),
+			"!" + path.posix.join(this.base, `**/_*`).replace(/\\/g, '/'),
+		];
 
-      this.compile(srcFile, distFile);
+		const files = globby.sync(patterns, { onlyFiles: true });
+		this.assets = files.map((filePath) => {
+			const srcFile = new File(filePath);
+			const distFile = this.getDistFile(srcFile);
 
-      return distFile;
-    });
-  }
+			this.compile(srcFile, distFile);
 
-  /**
-   * Watch all relevant files for changes
-   *
-   * @param {boolean} usePolling
-   */
-  watch(usePolling = false) {
-    if (this.isBeingWatched) return;
+			return distFile;
+		});
+	}
 
-    const options = { usePolling, ignored: /(^|[\/\\])\../ };
-    this.watcher = chokidar
-      .watch(this.data.from, options)
-      .on("all", (eventName, filePath) => {
-        this.onChange(filePath, eventName);
-      });
-    this.isBeingWatched = true;
-  }
+	/**
+	 * Watch all relevant files for changes
+	 *
+	 * @param {boolean} usePolling
+	 */
+	watch(usePolling = false) {
+		if (this.isBeingWatched) return;
 
-  unwatch() {
-    if (!this.watcher) return;
-    this.watcher.close();
-    this.isBeingWatched = false;
-  }
+		const options = { usePolling, ignored: /(^|[\/\\])\../ };
+		this.watcher = chokidar
+			.watch(path.join(this.data.from, '**/*.njk'), options)
+			.on("all", (eventName, filePath) => {
+				this.onChange(filePath, eventName);
+			});
+		this.isBeingWatched = true;
+	}
 
-  /**
-   * Handle when a relevant source file is changed
-   *
-   * @param {string} updatedFilePath
-   * @param {string} type
-   */
-  onChange(updatedFilePath, type = "change") {
-    const srcFile = new File(updatedFilePath);
-    const distFile = this.getDistFile(srcFile);
-    const isPartial = this.isPartialFile(srcFile);
+	unwatch() {
+		if (!this.watcher) return;
+		this.watcher.close();
+		this.isBeingWatched = false;
+	}
 
-    switch (type) {
-      case "change":
-      case "add":
-        if (isPartial) {
-          this.run();
-        } else {
-          Log.feedback(
-            `Compiling ${srcFile.relativePath()} to ${distFile.relativePath()}`
-          );
-          this.compile(srcFile, distFile);
-        }
-        break;
-      case "unlink":
-      case "unlinkDir":
-        if (isPartial) {
-          this.run();
-        } else {
-          distFile.delete();
-        }
-        break;
-    }
-  }
+	/**
+	 * Handle when a relevant source file is changed
+	 *
+	 * @param {string} updatedFilePath
+	 * @param {string} type
+	 */
+	onChange(updatedFilePath, type = "change") {
+		const srcFile = new File(updatedFilePath);
+		const distFile = this.getDistFile(srcFile);
+		const isPartial = this.isPartialFile(srcFile);
 
-  /**
-   * Compile template file
-   *
-   * @param {File} srcFile
-   * @param {File} distFile
-   */
-  compile(srcFile, distFile) {
-    const data = Object.assign({}, this.options.data);
-    let template = srcFile.read();
-    const frontmatter = fm(template);
+		switch (type) {
+			case "change":
+			case "add":
+				if (isPartial) {
+					this.run();
+				} else {
+					Log.feedback(
+						`Compiling ${srcFile.relativePath()} to ${distFile.relativePath()}`
+					);
+					this.compile(srcFile, distFile);
+				}
+				break;
+			case "unlink":
+			case "unlinkDir":
+				if (isPartial) {
+					this.run();
+				} else {
+					distFile.delete();
+				}
+				break;
+		}
+	}
 
-    if (frontmatter.attributes && Object.keys(frontmatter.attributes).length) {
-      if (srcFile.extension() === ".md") {
-        template = marked(frontmatter.body);
-      }
+	/**
+	 * Compile template file
+	 *
+	 * @param {File} srcFile
+	 * @param {File} distFile
+	 */
+	compile(srcFile, distFile) {
+		const data = Object.assign({}, this.options.data);
+		let template = srcFile.read();
+		const frontmatter = fm(template);
 
-      Object.assign(data, { page: frontmatter.attributes });
+		if (frontmatter.attributes && Object.keys(frontmatter.attributes).length) {
+			if (srcFile.extension() === ".md") {
+				template = marked(frontmatter.body);
+			}
 
-      if (data.page.layout) {
-        template =
-          '{% extends "' +
-          data.page.layout +
-          '.njk" %}\n{% block ' +
-          this.options.block +
-          " %}" +
-          template +
-          "\n{% endblock %}";
-      } else {
-        Log.info(`No layout declared in ${srcFile.relativePath()}`);
-      }
-    }
+			Object.assign(data, { page: frontmatter.attributes });
 
-    const result = this.compiler.renderString(template, data);
-    distFile.makeDirectories();
-    distFile.write(result);
-  }
+			if (data.page.layout) {
+				template =
+					'{% extends "' +
+					data.page.layout +
+					'.njk" %}\n{% block ' +
+					this.options.block +
+					" %}" +
+					template +
+					"\n{% endblock %}";
+			} else {
+				Log.info(`No layout declared in ${srcFile.relativePath()}`);
+			}
+		}
+
+		const result = this.compiler.renderString(template, data);
+		distFile.makeDirectories();
+		distFile.write(result);
+	}
 }
 
 module.exports = NunjucksTask;
